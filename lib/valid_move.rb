2595f1212870dc8cb5c3eec037fa1e27be47a92f
JOSIAH E MODESTE <ephraimmo2297@gmail.com>
2019-12-15 16:09:18 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -4,4 +4,4 @@ def position_taken
 end
 
 
-# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
